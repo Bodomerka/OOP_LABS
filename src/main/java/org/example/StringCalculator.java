62482cf40c98d799d3ef2ec7a86648755b148b75
Bodomerka <117486637+Bodomerka@users.noreply.github.com>
2023-11-09 19:27:46 +0200

    крок 7

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -2,6 +2,8 @@ package org.example;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
@@ -9,31 +11,35 @@ public class StringCalculator {
         if (numbers.isEmpty()) {
             return 0;
         }
+
         List<String> delimiters = new ArrayList<>();
         delimiters.add(",");
         delimiters.add("\n");
+
+        // Перевірка на наявність визначеного користувачем роздільника
         if (numbers.startsWith("//")) {
             int delimiterIndex = numbers.indexOf("\n");
             if (delimiterIndex != -1) {
-                String customDelimiter = numbers.substring(2, delimiterIndex);
-                delimiters.add(customDelimiter);
-                numbers = numbers.substring(delimiterIndex + 1);
+                String delimiterSection = numbers.substring(2, delimiterIndex);
+                Matcher m = Pattern.compile("\\[(.*?)\\]").matcher(delimiterSection);
+                while (m.find()) {
+                    delimiters.add(Pattern.quote(m.group(1))); // Додаємо усі роздільники між квадратними дужками
+                }
+                numbers = numbers.substring(delimiterIndex + 1); // Оновлюємо рядок чисел, видаляючи частину з роздільником
             }
         }
+
+        // Перевірка на роздільники, які йдуть підряд
         for (String delimiter : delimiters) {
-            numbers = numbers.replace(delimiter, ",");
+            if (numbers.contains(delimiter + delimiter)) {
+                throw new IllegalArgumentException("Некоректний ввід: введення не повинно містити два роздільники підряд");
+            }
         }
 
-        // Перевірка на два роздільники підряд
-        if (numbers.contains(",,") || numbers.contains(",\n") || numbers.contains("\n,")) {
-            throw new IllegalArgumentException("Некоректний ввід: введення не повинно містити два роздільники підряд");
-        }
+        // Заміна всіх роздільників на стандартний для спрощення подальшого розділення
+        String regex = String.join("|", delimiters);
+        String[] numberArray = numbers.split(regex); // Розділяємо вхідний рядок за допомогою всіх роздільників
 
-        if (numbers.endsWith(",\n") || numbers.endsWith("\n,") || numbers.endsWith(",")) {
-            throw new IllegalArgumentException("Некоректний ввід: введення не повинно закінчуватися на ',\\n', '\\n,' або ','");
-        }
-
-        String[] numberArray = numbers.split(",");
         int sum = 0;
         List<Integer> negativeNumbers = new ArrayList<>();
         for (String numberStr : numberArray) {
@@ -41,14 +47,16 @@ public class StringCalculator {
                 int number = Integer.parseInt(numberStr.trim());
                 if (number < 0) {
                     negativeNumbers.add(number);
-                } else if (number <= 1000) {
+                } else if (number <= 1000) { // Ігноруємо числа більше 1000
                     sum += number;
                 }
             }
         }
+
         if (!negativeNumbers.isEmpty()) {
             throw new IllegalArgumentException("Недозволені від’ємні числа: " + negativeNumbers.toString());
         }
+
         return sum;
     }
 }
